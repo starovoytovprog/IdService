10b20cde04aa2c7dccd23c99a6e21821e079e7db
StarovoytovProg <adminofusa@gmail.com>
2020-04-25 19:58:40 +0300

    Hello world!

---
+++ src/main/java/ru/starovoytov/MainClass.java
@@ -0,0 +1,13 @@
+package ru.starovoytov;
+
+/**
+ * Стартовый класс сервиса
+ *
+ * @author starovoytov
+ * @since 2020.04.25
+ */
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
